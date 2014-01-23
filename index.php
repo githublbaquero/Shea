6ac70cca4970041120238de2c3ad6e3b29e6195c
githublbaquero <LinaB@LinaBs-MacBook-Pro.local>
2014-01-23 09:29:32 -0500

    Sche Website
    Sche Website

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php # index.php
+$page_title = 'Schea Communications';
+
+include ('./includes/header.html');
+
+/*include('./includes/aboutus.html');
+
+include('./includes/services.html');
+
+include('./includes/staff.html');
+
+include('./includes/ourwork.html');
+
+include('./includes/contactus.html');
+
+include ('./includes/footer.html');*/
+
+?>
